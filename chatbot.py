0ce8f6b55859869be78d3499dd49a47a4ee23651
Jimmy Wang <jimmy.w@aliyun.com>
2023-12-29 12:43:55 +0800

    style: add color to the input prompt

--- chatbot.py
+++ chatbot.py
@@ -38,9 +38,23 @@ conversation = LLMChain(llm=chat,
                         verbose=verbose,
                         memory=memory)  # 使用 conversation, 结果: string
 
+
+class Color:
+    PURPLE = '\033[95m'
+    CYAN = '\033[96m'
+    DARKCYAN = '\033[36m'
+    BLUE = '\033[94m'
+    GREEN = '\033[92m'
+    YELLOW = '\033[93m'
+    RED = '\033[91m'
+    BOLD = '\033[1m'
+    UNDERLINE = '\033[4m'
+    END = '\033[0m'
+
+
 while True:
     try:
-        message = input("✎✎ ")
+        message = input(f"{Color.BLUE}✎✎✎ {Color.END}")
         if message.lower() == "exit":
             exit()
         result = conversation({
